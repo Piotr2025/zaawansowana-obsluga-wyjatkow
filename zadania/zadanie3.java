7daedd50a1fd1a2c24831c5a8affcde4ea3417da
3nterUsrN4me <pjozefczyk@student.agh.edu.pl>
2026-06-11 20:12:39 +0200

    doprecyzowanie

---
+++ zadania/zadanie3.java
@@ -0,0 +1,16 @@
+// Symulacja wyjątków biznesowych 
+class DatabaseException extends Exception { public DatabaseException(String m, Throwable c) { super(m, c); } }
+class UserNotFoundException extends RuntimeException { public UserNotFoundException(String m, Throwable c) { super(m, c); } }
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // Niska warstwa rzuciła błąd bazy danych:
+            throw new DatabaseException("FATAL: password authentication failed", null);
+        } 
+        // TODO: Złap DatabaseException a następnie zrób wyjątek UserNotFoundException (patrz slajd 11),
+        //       przekazując oryginalny wyjątek jako drugi argument, aby zachować oryginalny Stack Trace.
+        catch (DatabaseException e) {
+        }
+    }
+}
