4f8b20e6ee82332c0b4ae504e2c83908863c4802
3nterUsrN4me <pjozefczyk@student.agh.edu.pl>
2026-06-11 20:10:57 +0200

    doprecyzowanie

---
+++ zadania/zadanie1.py
@@ -0,0 +1,9 @@
+print("--- Start ---")
+
+tekst = "trzy"
+
+# TODO: Otwórz blok 'try' i skonwertuj tekst.
+# TODO: Użyj bloku 'else' żeby wypisać komunikat o sukcesie TYLKO, gdy błąd nie wystąpił.
+# TODO: Użyj except ValueError, aby obsłużyć błąd odpowiednim komunikatem.
+
+print("--- Koniec programu ---")
